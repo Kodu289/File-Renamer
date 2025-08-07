9c3d73509377a4c10fbdac9bcd660e594358003d
Kodu289 <hlin1080@gmail.com>
2025-08-07 18:30:05 -0400

    made a config file and general setup

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 import tkinter as tk
+from config import *
+import os
 
+class File_Renamer:
+    def __init__(self):
+        window = tk.Tk()
+        window.title(WINDOW_TITLE)
+        window.geometry(WINDOW_SIZE)
+        window.configure(bg=WINDOW_BG_COLOUR)
+
+        self.setup_widget() # call upon the widgets created
+        window.mainloop()
+
+    def setup_widget(self):
+        self.label = tk.label
+        print("widget")
+
+    def rename_file(self):
+        print("done")
+
+
+app = File_Renamer()
