02238c39b3bb32938a535239db410543fc341e11
Kodu289 <hlin1080@gmail.com>
2025-08-07 20:52:39 -0400

    added a bunch of stuff

--- main.py
+++ main.py
@@ -4,20 +4,36 @@ import os
 
 class File_Renamer:
     def __init__(self):
-        window = tk.Tk()
-        window.title(WINDOW_TITLE)
-        window.geometry(WINDOW_SIZE)
-        window.configure(bg=WINDOW_BG_COLOUR)
+        self.window = tk.Tk()
+        self.window.title(WINDOW_TITLE)
+        self.window.geometry(WINDOW_SIZE)
+        self.window.configure(bg=WINDOW_BG_COLOUR)
 
+        
         self.setup_widget() # call upon the widgets created
-        window.mainloop()
+        self.load_txt()
+        self.write_to_txt()
+        self.window.mainloop()
 
     def setup_widget(self):
-        self.label = tk.label
-        print("widget")
+        self.text_box = tk.Text(self.window, height=5, width=50)
+        self.text_box.place(x=10,y=10)
+
+        self.button = tk.Button(self.window, text="save text", command=self.write_to_txt)
+        self.button.place(x=10,y=100)
 
     def rename_file(self):
         print("done")
 
+    def write_to_txt(self):
+        text_to_save = self.text_box.get("1.0", tk.END)
+        with open("pathfile.txt","w") as file:
+            file.write(text_to_save)
+
+    def load_txt(self):
+        if os.path.exists("pathfile.txt"):
+            with open("pathfile.txt", "r") as file:
+                content = file.read()
+                self.text_box.insert(tk.END, content)
 
 app = File_Renamer()
